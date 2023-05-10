d8aa96ea52ced44ad2a690c1e72440e78841a59b
ghergomathias@hotmail.com <ghergomathias@hotmail.com>
2023-05-10 14:45:36 -0300

    Creacion de un proyecto personal para una empresa de copiados y distribucion de rubricas contables.

---
+++ slider.js
@@ -0,0 +1,20 @@
+const sliderItems = document.querySelectorAll('.slider-item');
+let currentItem = 0;
+
+function startSlider() {
+  resetSlider();
+  sliderItems[currentItem].classList.add('active');
+  currentItem++;
+  if (currentItem >= sliderItems.length) {
+    currentItem = 0;
+  }
+  setTimeout(startSlider, 5000);
+}
+
+function resetSlider() {
+  for (let i = 0; i < sliderItems.length; i++) {
+    sliderItems[i].classList.remove('active');
+  }
+}
+
+startSlider();
